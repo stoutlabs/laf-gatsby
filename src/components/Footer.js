1624e68fb3b78ed6bbf38a845d4d5435dddc98e8
Daniel <daniel@stoutlabs.com>
2018-08-20 00:19:39 -0400

    Adjust footer styles for better accessibility scores

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,27 +3,27 @@ import styled from "styled-components";
 import { SocialIcons } from "./SocialIcons";
 
 const StyledFooter = styled.footer`
-   text-align: center;
-   color: #999;
-   font-size: 0.8rem;
-   letter-spacing: 2px;
-   margin-bottom: 1rem;
+  text-align: center;
+  color: #888;
+  font-size: 0.85rem;
+  letter-spacing: 2px;
+  margin-bottom: 1rem;
 
-   @media screen and (max-width: 600px) {
-      font-size: 0.75rem;
-   }
+  @media screen and (max-width: 600px) {
+    font-size: 0.8rem;
+  }
 `;
 
 export const Footer = () => {
-   return (
-      <StyledFooter>
-         <p>
-            &copy; <span id="curyear">{new Date().getFullYear()}</span> LETA AUSTIN FOSTER &
-            ASSOCIATES, INC
-         </p>
-         <SocialIcons />
-      </StyledFooter>
-   );
+  return (
+    <StyledFooter>
+      <p>
+        &copy; <span id="curyear">{new Date().getFullYear()}</span> LETA AUSTIN FOSTER & ASSOCIATES,
+        INC
+      </p>
+      <SocialIcons />
+    </StyledFooter>
+  );
 };
 
 export default Footer;
